f3cbabfdfcc273ff75e49708eed3bc78efb019fb
thaisclxt <thaisfc2000@gmail.com>
2023-06-09 16:01:06 -0300

    refactor: simplificação de loops

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import time
 import utils
 import numpy as np
+
 from sympy import parse_expr, Expr
 from function import Function
 from methods.MG import MG
@@ -37,20 +39,26 @@ def main():
 
     print()
 
-    if is_MG:
-        for i in range(4):
-            tolerance: float = 10**-(i+2)
+    extern_range = 1 if is_MG else 3
+    table_rows = 4
+    total_tests = 1
+
+    start_time = time.time()
+
+    for index in range(extern_range):
+        for row in range(table_rows):
+            tolerance: float = 10**-(row+2)
             result_list = []
             k_list = []
             time_list = []
 
-            for _ in track(range(100), description='Processando...'):
-                mg = MG(func, tolerance)
-                mg.algorithm()
+            for _ in track(range(total_tests), description="Processando..."):
+                method = MG(func, tolerance) if is_MG else MGRP(func, tolerance, index)
+                method.algorithm()
 
-                time_list.append(mg.processing_time)
-                result_list.append(mg.xk)
-                k_list.append(mg.k)
+                time_list.append(method.processing_time)
+                result_list.append(method.xk)
+                k_list.append(method.k)
 
             result_mean = calculate_result_mean(result_list)
 
@@ -60,38 +68,18 @@ def main():
             x_row.append(str(result_mean))
             norm_row.append(str(norm(result_mean, x_star)))
 
-        mg.generate_table(tolerance_row, time_row, k_row, x_row, norm_row)
+            if not is_MG:
+                lambda_row.append(f'{index} + 1 + (1 / (k+1))')
+        
+        print()
 
+    if is_MG:
+        method.generate_table(tolerance_row, time_row, k_row, x_row, norm_row)
     else:
-        for aa in range(3):
-            for i in range(4):
-                tolerance: float = 10**-(i+2)
-                result_list = []
-                k_list = []
-                time_list = []
+        method.generate_table(lambda_row, tolerance_row, time_row, k_row, x_row, norm_row)
 
-                for _ in track(range(100), description='Processando...'):
-                    mgrp = MGRP(func, tolerance, aa)
-                    mgrp.algorithm()
-
-                    time_list.append(mgrp.processing_time)
-                    result_list.append(mgrp.xk)
-                    k_list.append(mgrp.k)
-
-                result_mean = calculate_result_mean(result_list)
-
-                tolerance_row.append(str(tolerance))
-                lambda_row.append(f'{aa} + 1 + (1 / (k+1))')
-                time_row.append(str(np.mean(time_list)))
-                k_row.append(str(np.mean(k_list)))
-                x_row.append(str(result_mean))
-                norm_row.append(str(norm(result_mean, x_star)))
-
-            print()
-
-        mgrp.generate_table(lambda_row, tolerance_row,
-                            time_row, k_row, x_row, norm_row)
-
+    end_time = int(time.time() - start_time)
+    print(f'\nTempo de processamento total: {end_time // 60}min {end_time % 60}sec')
 
 if __name__ == "__main__":
     main()
