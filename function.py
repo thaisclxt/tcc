5755314f075cd61ea5d54f46e58f30c3b58bf5a4
thaisclxt <thais.calixto@mail.uft.edu.br>
2023-06-26 17:41:49 -0300

    docs: correção

--- function.py
+++ function.py
@@ -4,32 +4,36 @@ from sympy import symbols, Expr, Derivative
 
 
 class Function:
-    """Uma classe que representa função de entrada `f(x,y)` que será usada no algoritmo."""
+    """Uma classe que representa função de entrada f(x,y) que será usada no algoritmo."""
 
     def __init__(self, is_user_input: bool, expression: Expr, global_minimun: np.ndarray):
         self.is_user_input = is_user_input
-        """Verifica se a função `f(x,y)` foi digitada pelo usuário ou se foi excolhida entre as funções de exemplo do TCC 1."""
+        """Verifica se a função f(x,y) foi digitada pelo usuário ou se foi escolhida entre as funções de exemplo."""
 
         self.expression = expression
-        """È a função de entrada em formato de uma expressão SymPy."""
+        """É a função de entrada em formato de uma expressão SymPy."""
 
         self.global_minimun = global_minimun
-        """É o ponto de mpinimo global da função f(x,y)."""
+        """É o ponto de mínimo global da função f(x,y)."""
 
         self.x, self.y, self.alpha = symbols('x y alpha')
         """São os símbolos usados ao decorrer do programa de matemática simbólica."""
 
         self.gradient = self.calculate_gradient()
-        """O vetor gradiente da função `f(x,y)`."""
+        """O vetor gradiente da função f(x,y)."""
 
     def calculate_gradient(self) -> list[Expr]:
-        """Calcula o vetor gradiente da função `f(x,y)`.
+        """Calcula o vetor gradiente da função f(x,y).
 
         Explicação
         ----------
         O vetor gradiente é dado pela derivada de `f(x,y)` em relação à `x` e pela derivada de f(x,y) em relação à `y`.
-        """
 
+        Retorno
+        -------
+        n : `list[Expr]`
+            O vetor gradiente da função f(x,y).
+        """
         grad_y = Derivative(self.expression, self.y, evaluate=True)
         grad_x = Derivative(self.expression, self.x, evaluate=True)
 
